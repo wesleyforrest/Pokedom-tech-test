1f00c11244f6a72524acf62de1bef9ec1e2f4704
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-05-25 15:50:19 +0100

    added pokemon object, and got code working

--- script.js
+++ script.js
@@ -2,11 +2,39 @@ import pokemonArray from "./data/pokemon.js";
 
 //console.log(pokemonArray[0]);
 
-const pokemonCard = {
-  id: `${pokemonArray[0]}`,
-  name: `${pokemonArray[1]}`,
-  types: `${pokemonArray[2]}`,
-  sprites: `${pokemonArray[3]}`,
+const newPokemon = pokemonArray
+  .map((pokemon) => {
+    return `<div class="card">
+  <div class="card__content">
+      <img class="card__image" src="${pokemon.sprite}" />
+  
+      <div class="card__heading">${pokemon.name}</div>
+      <p class="card__text">${pokemon.name}${pokemon.id} is a ${pokemon.types}</p>
+  </div>
+</div>`;
+  })
+  .join("");
+const cardContainer = document.querySelector(".card-container");
+
+const individualPokemon = {
+  id: `${pokemonArray[0].id}`,
+  name: `${pokemonArray[0].name}`,
+  types: `${pokemonArray[0].types}`,
+  sprite: `${pokemonArray[0].sprite}`,
 };
 
-console.log(pokemonCard);
+console.log(pokemonArray[0]);
+
+const pokemonContainer = (individualPokemon) => {
+  return `
+    <div class="card">
+        <div class="card__content">
+            <img class="card__image" src="${individualPokemon.sprite}" />
+        
+            <div class="card__heading">${individualPokemon.name}</div>
+            <p class="card__text">${individualPokemon.name}${individualPokemon.id} is a ${individualPokemon.types}</p>
+        </div>
+    </div>`;
+};
+
+cardContainer.innerHTML = newPokemon;
