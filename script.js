74591b1c7ebdd917e95833ad9c32b49100f408f0
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-05-25 13:02:04 +0100

    Imported pokemon.js to main js file and checked import works

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello");
+import pokemonArray from "./data/pokemon.js";
+
+//console.log(pokemonArray[0]);
+
+const pokemonCard = {
+  id: `${pokemonArray[0]}`,
+  name: `${pokemonArray[1]}`,
+  types: `${pokemonArray[2]}`,
+  sprites: `${pokemonArray[3]}`,
+};
+
+console.log(pokemonCard);
